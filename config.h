b766f536041ab19903dfc7d24189514bbd919eb7
procrastimax <heykeroth.dev@gmail.com>
2020-10-13 20:32:07 +0200

    Making st_float terminal centered on spawn

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     iscentered  isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            0,            1,           -1 },
-	{ "st_float", NULL,       NULL,       0,            0,            1,           -1 },
+	{ "st_float", NULL,       NULL,       0,            1,            1,           -1 },
 };
 
 /* layout(s) */
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-b", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *term_floatcmd[]  = { "st", "-c", "st_float", "-g", "120x30+280+150",  NULL };
+static const char *term_floatcmd[]  = { "st", "-c", "st_float", "-g", "120x30",  NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
 static const char *keepasscmd[]  = { "keepassxc", NULL };
 
